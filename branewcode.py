d56f4ed425af871714a51f912c13acdb9318c234
benappu1988-a11y <appuvincent86@gmail.com>
2026-06-14 17:20:02 +0100

    my first ever coding lesson with Kavya Shree

--- branewcode.py
+++ branewcode.py
@@ -1,1 +1,9 @@
 print("hello world good morning",19*105)
+print("welcome to the world of programming!")
+#PRINT A NUMBER
+print(4)
+#next line after print
+print("hello python \n")
+#multiple arguments
+print("this is the best programming class","10years old",10)
+print("Vincent",end="|")
