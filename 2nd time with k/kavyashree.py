5f52db8dcac16f9c6b27aef27ed307a2da50b85b
benappu1988-a11y <appuvincent86@gmail.com>
2026-06-25 17:39:06 +0100

    my integer and float and bool and string and typecasting

---
+++ 2nd time with k/kavyashree.py
@@ -0,0 +1,8 @@
+vincent=0123456.6543210
+print("\n",vincent, "\n")
+#now i will print the data type
+print(type(vincent))
+#changing the type of vincent to INTEGER
+KAYVA=int(0123456.6543210)
+print(KAYVA)
+print(type(KAYVA))
