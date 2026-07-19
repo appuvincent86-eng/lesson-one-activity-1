1dfebe8ec38edb4421077ea304c7c2f38d1b8e95
appuvincent86-eng <appuvincent86@gmail.com>
2026-07-19 17:30:30 +0100

    Add number swap program in numbswap.py
    bad boy

---
+++ numbswap.py
@@ -0,0 +1,15 @@
+# Number swap program
+
+a = 10
+b = 20
+
+print("Before swap:")
+print("a =", a)
+print("b =", b)
+
+# Swap the values
+a, b = b, a
+
+print("\nAfter swap:")
+print("a =", a)
+print("b =", b)
