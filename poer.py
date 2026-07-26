de5abfbae9f39f128204757c81cf2ceebdeea591
appuvincent86-eng <appuvincent86@gmail.com>
2026-07-26 17:35:19 +0100

    Create poer.py
    bad boy

---
+++ poer.py
@@ -0,0 +1,9 @@
+v5=bool(input("Enter question: "))
+v4=int(input("Enter answer: "))
+sum=True
+if v5==True:
+    print("You entered True")
+elif v5==False:
+    print("You entered False")
+else:
+    print("your answer as no link to the question")
