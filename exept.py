5b97d5b5648a047292f2949e93ca064e47953944
appuvincent86-eng <appuvincent86@gmail.com>
2026-09-13 16:40:18 +0100

    Add error handling in exept.py
    bbbbbbbbbbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaddddddddddd

---
+++ exept.py
@@ -0,0 +1,8 @@
+try:
+ contanat = int(input("enter a name"))
+except ValueError:
+ print("your gooooooooooooingggggggggggg to jaaaaaaaaaaillllllllllllllllllllll!")
+except:
+ print("you are going to jail")
+finally:
+    print("this is the end of the program")
